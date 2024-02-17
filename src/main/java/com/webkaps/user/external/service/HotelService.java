1435cf8c23d9e4e42691df7832bdf8b1b1ff1e7f
Khushal <khushal.singh07@gmail.com>
2024-02-17 18:38:05 +0530

    Fein Client implementation in User Service for getting Hotel Service detail (2)

---
+++ src/main/java/com/webkaps/user/external/service/HotelService.java
@@ -0,0 +1,13 @@
+package com.webkaps.user.external.service;
+
+import com.webkaps.user.dto.Hotel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "HOTEL-SERVICE")
+public interface HotelService {
+
+    @GetMapping("/hotel-service/{hotelId}")
+    Hotel getHotel(@PathVariable  String hotelId);
+}
